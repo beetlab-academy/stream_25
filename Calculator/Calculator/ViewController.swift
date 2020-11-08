f56a7cb28a5b8d3cce660d85f3d19b32ac21808d
Никита <nikita.a.semenov@gmail.com>
2020-11-08 14:34:20 +0300

    08.11

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Krasa on 08.11.2020.
+//  Copyright © 2020 Contractors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
